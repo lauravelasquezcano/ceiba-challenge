1993d7882b149e7e74910fe18b3d60c4d7753bd9
Laura Velásquez Cano <laura.velasquez.cano@gmail.com>
2022-07-12 10:49:07 -0500

    Creating users list view

--- app/src/main/java/com/lauravelasquezcano/ceiba/app/ui/main/MainActivity.kt
+++ app/src/main/java/com/lauravelasquezcano/ceiba/app/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.lauravelasquezcano.ceiba
+package com.lauravelasquezcano.ceiba.app.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lauravelasquezcano.ceiba.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
